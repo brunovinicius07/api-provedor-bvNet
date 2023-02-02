3bdba32bec5f93b164ac675a4feb1b86ca154aea
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-02-02 16:39:50 -0300

    Criado o endpoint (cadastrarCliente)

--- src/main/java/com/api/provedorbvNET/services/ClienteService.java
+++ src/main/java/com/api/provedorbvNET/services/ClienteService.java
@@ -1,6 +1,9 @@
 package com.api.provedorbvNET.services;
 
+import com.api.provedorbvNET.dtos.ClienteDto;
+import com.api.provedorbvNET.entities.Cliente;
 import com.api.provedorbvNET.repositories.ClienteRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,17 @@ public class ClienteService {
 
     @Autowired
     ClienteRepository clienteRepository;
+
+    @Transactional
+    public Cliente cadastrarCliente(ClienteDto clienteDto) {
+        Cliente cliente = clienteRepository.save(converteObjetoDto(clienteDto));
+        return cliente;
+    }
+
+    private Cliente converteObjetoDto(ClienteDto clienteDto) {
+        return new Cliente(clienteDto.getNome(),
+                           clienteDto.getEndereco(),
+                           clienteDto.getTelefone(),
+                           clienteDto.getEmail());
+    }
 }

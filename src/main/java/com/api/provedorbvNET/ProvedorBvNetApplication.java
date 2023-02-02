f0081cdeece46b5653a0cf715c3fae2c83c93c4c
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-02-01 21:40:55 -0300

    Criada a api

---
+++ src/main/java/com/api/provedorbvNET/ProvedorBvNetApplication.java
@@ -0,0 +1,13 @@
+package com.api.provedorbvNET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvedorBvNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvedorBvNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/provedorbvNET/ProvedorBvNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.provedorbvNET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvedorBvNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

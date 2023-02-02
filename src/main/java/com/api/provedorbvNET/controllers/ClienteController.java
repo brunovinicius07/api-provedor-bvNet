04b341d91d379feaeb43a5ee9d2a63c2e9e45aea
brunovinicius07 <brunoviniciusoliveiratavares@gmail.com>
2023-02-02 16:38:25 -0300

    Criado o método (cadastraarCliente)

--- src/main/java/com/api/provedorbvNET/controllers/ClienteController.java
+++ src/main/java/com/api/provedorbvNET/controllers/ClienteController.java
@@ -1,14 +1,23 @@
 package com.api.provedorbvNET.controllers;
 
+import com.api.provedorbvNET.dtos.ClienteDto;
+import com.api.provedorbvNET.entities.Cliente;
 import com.api.provedorbvNET.services.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
 
     @Autowired
-    ClienteService clienteService;
+    private ClienteService clienteService;
+
+    @PostMapping(value = "/cadastrarCliente")
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody @Valid ClienteDto clienteDto) {
+        return new ResponseEntity<Cliente>(clienteService.cadastrarCliente(clienteDto), HttpStatus.CREATED);
+    }
 }
